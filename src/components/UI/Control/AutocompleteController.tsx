756327fabe363663206c9d426679914508738c9a
oranjevo <iandrew.jj@gmail.com>
2023-04-24 20:18:12 +0300

    add component styles, optional props

--- src/components/UI/Control/AutocompleteController.tsx
+++ src/components/UI/Control/AutocompleteController.tsx
@@ -1,41 +1,70 @@
 import { Control, Controller } from 'react-hook-form'
-import { Autocomplete, AutocompleteProps, TextField } from '@mui/material'
+import { Autocomplete, TextField } from '@mui/material'
 import GridItem from '@/components/UI/Grid/GridItem'
-import { FormFields } from '@/types'
+import { CustomAutocompleteProps, FormFields, FieldParams } from '@/types'
 import { FieldPath } from 'react-hook-form/dist/types'
+import { Grid2Props } from '@mui/material/Unstable_Grid2'
+import { makeStyles } from 'tss-react/mui'
 
-interface Props<T, F extends FormFields>
-    extends Omit<
-        AutocompleteProps<T, boolean | undefined, boolean | undefined, false>,
-        'renderInput'
-    > {
-    label: string
-    control: Control<F>
-    name: FieldPath<F>
+const useStyles = makeStyles()((theme) => ({
+    root: {
+        '& label': {
+            paddingLeft: theme.spacing(1),
+            fontSize: '15px'
+        },
+        '& input': {
+            paddingLeft: `${theme.spacing(2)} !important`,
+            fontSize: '14px'
+        }
+    }
+}))
+
+interface Props<T, Fields extends FormFields> extends CustomAutocompleteProps<T> {
+    fieldParams: FieldParams
+    control: Control<Fields>
+    name: FieldPath<Fields>
+    helperText?: string
+    gridProps?: Grid2Props
 }
 
-const AutocompleteController = <T, F extends FormFields = FormFields>({
+const AutocompleteController = <T, Fields extends FormFields = FormFields>({
     name,
     control,
-    label,
-    ...props
-}: Props<T, F>) => {
+    fieldParams,
+    helperText,
+    gridProps,
+    ...autocompleteProps
+}: Props<T, Fields>) => {
+    const { LABEL, PLACEHOLDER } = fieldParams
+    const { classes } = useStyles()
     return (
         <Controller
             name={name}
             control={control}
-            render={({ field: { value, onChange, ...fieldProps } }) => (
-                <GridItem md={6}>
+            render={({
+                field: { value, onChange, ...controlProps },
+                fieldState: { error, invalid }
+            }) => (
+                <GridItem sm={6} {...gridProps}>
                     <Autocomplete
-                        {...fieldProps}
-                        {...props}
-                        noOptionsText={'Нет совпадений'}
                         fullWidth
+                        className={classes.root}
+                        autoComplete={false}
+                        noOptionsText={'Нет совпадений'}
                         inputValue={value || ''}
                         onInputChange={(_, value) => onChange(value)}
                         renderInput={(params) => (
-                            <TextField variant={'standard'} label={label} {...params} />
+                            <TextField
+                                variant={'standard'}
+                                label={LABEL}
+                                error={invalid}
+                                helperText={invalid ? error?.message : helperText}
+                                placeholder={PLACEHOLDER}
+                                {...params}
+                            />
                         )}
+                        {...autocompleteProps}
+                        {...controlProps}
                     />
                 </GridItem>
             )}
