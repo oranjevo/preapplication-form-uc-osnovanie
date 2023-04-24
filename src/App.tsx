228769f3e80e70f757ed5ded1b4d7e010190a4d0
oranjevo <iandrew.jj@gmail.com>
2023-04-24 20:16:07 +0300

    add mui theme provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,31 @@
 import './index.sass'
 import OsnovanieForm from '@/components/OsnovanieForm'
+import { createTheme, ThemeProvider } from '@mui/material'
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: '#15203a'
+        }
+    },
+    typography: {
+        fontFamily: 'Verdana'
+    },
+    components: {
+        MuiFormHelperText: {
+            styleOverrides: {
+                root: { position: 'absolute', bottom: '-1.5rem', left: '5px' }
+            }
+        }
+    }
+})
 
 const App = () => {
-    return <OsnovanieForm />
+    return (
+        <ThemeProvider theme={theme}>
+            <OsnovanieForm />
+        </ThemeProvider>
+    )
 }
 
 export default App
