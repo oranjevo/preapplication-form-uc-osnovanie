c3eff847ee53041988aed0a3d1d51a4d4d9898e1
oranjevo <iandrew.jj@gmail.com>
2023-04-24 20:22:56 +0300

    add constant

---
+++ src/constants/FIELDS.ts
@@ -0,0 +1,42 @@
+export const FIELDS = {
+    CITY: {
+        LABEL: 'Город',
+        PLACEHOLDER: 'Москва'
+    },
+    COMMENT: {
+        LABEL: 'Комментарий',
+        PLACEHOLDER: undefined
+    },
+    EMAIL: {
+        LABEL: 'Email',
+        PLACEHOLDER: 'email@domain.ru'
+    },
+    FIRSTNAME: {
+        LABEL: 'Имя',
+        PLACEHOLDER: 'Иван'
+    },
+    INN: {
+        LABEL: 'ИНН',
+        PLACEHOLDER: '000000000000'
+    },
+    LASTNAME: {
+        LABEL: 'Фамилия',
+        PLACEHOLDER: 'Иванов'
+    },
+    PARTY_TYPE: {
+        LABEL: 'Получатель ЭП',
+        PLACEHOLDER: undefined
+    },
+    PHONE: {
+        LABEL: 'Телефон',
+        PLACEHOLDER: '+7 (000) 00 00 00'
+    },
+    PRODUCTS: {
+        LABEL: 'Тариф',
+        PLACEHOLDER: undefined
+    },
+    REGION: {
+        LABEL: 'Регион',
+        PLACEHOLDER: undefined
+    }
+}
