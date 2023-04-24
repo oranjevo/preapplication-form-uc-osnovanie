e457f06e6fc99a6a20fb30ad371322ac14ebd5b7
oranjevo <iandrew.jj@gmail.com>
2023-04-24 20:19:31 +0300

    add component styles, optional props

--- src/components/UI/Control/SelectController.tsx
+++ src/components/UI/Control/SelectController.tsx
@@ -1,59 +1,103 @@
 import { Control, Controller } from 'react-hook-form'
-import { FormControl, InputLabel, MenuItem, Select } from '@mui/material'
+import {
+    FormControl,
+    FormHelperText,
+    InputLabel,
+    MenuItem,
+    MenuItemProps,
+    Select,
+    SelectProps,
+    Typography
+} from '@mui/material'
 import GridItem from '@/components/UI/Grid/GridItem'
 import { FieldPath } from 'react-hook-form/dist/types'
-import { FormFields } from '@/types'
+import { FormFields, FieldParams, TMenuItem } from '@/types'
+import PriceHolder from '@/components/UI/PriceHolder'
+import { Grid2Props } from '@mui/material/Unstable_Grid2'
+import { makeStyles } from 'tss-react/mui'
 
-type TMenuItem = {
-    value: string
-    text: string
-}
-interface Props<MenuItem extends TMenuItem, T extends FormFields, TName extends FieldPath<T>> {
-    label: string
+const useStyles = makeStyles()((theme) => ({
+    label: { paddingLeft: theme.spacing(1), fontSize: '14px' },
+    select: { paddingLeft: theme.spacing(2), fontSize: '14px' },
+    menuItem: { position: 'relative', fontSize: '14px' }
+}))
+
+interface Props<
+    MenuItem extends TMenuItem,
+    Fields extends FormFields,
+    TName extends FieldPath<Fields>
+> extends SelectProps {
+    fieldParams: FieldParams
     menuItems: MenuItem[]
-    control: Control<T>
+    control: Control<Fields>
     name: TName
+    helperText?: string
+    menuItemProps?: MenuItemProps
+    gridProps?: Grid2Props
 }
 
 const SelectController = <
     MenuItem extends TMenuItem = TMenuItem,
-    T extends FormFields = FormFields,
-    TName extends FieldPath<T> = FieldPath<T>
+    Fields extends FormFields = FormFields,
+    TName extends FieldPath<Fields> = FieldPath<Fields>
 >({
     name,
     control,
-    label,
-    menuItems
-}: Props<MenuItem, T, TName>) => {
+    fieldParams,
+    menuItems,
+    helperText,
+    menuItemProps,
+    gridProps,
+    ...selectProps
+}: Props<MenuItem, Fields, TName>) => {
+    const { LABEL, PLACEHOLDER } = fieldParams
+    const { classes } = useStyles()
     return (
         <Controller
             name={name}
             control={control}
-            render={({ field: { ref, value, ...fieldProps } }) => (
-                <GridItem md={6}>
+            render={({
+                field: { ref, value, ...controlProps },
+                fieldState: { error, invalid }
+            }) => (
+                <GridItem sm={6} {...gridProps}>
                     <FormControl variant={'standard'} fullWidth>
-                        <InputLabel id={`select-${name}-id`}>{label}</InputLabel>
+                        <InputLabel
+                            id={`select-${name}-id`}
+                            error={invalid}
+                            className={classes.label}
+                        >
+                            {LABEL}
+                        </InputLabel>
                         <Select
+                            fullWidth
+                            className={classes.select}
+                            value={value || ''}
                             labelId={`select-${name}-id`}
                             id={`select-${name}-id`}
-                            autoWidth
-                            label={label}
+                            error={invalid}
+                            placeholder={PLACEHOLDER}
                             inputRef={ref}
-                            value={value || ''}
-                            {...fieldProps}
+                            {...selectProps}
+                            {...controlProps}
                         >
                             {menuItems.map((menuItem: MenuItem, index: number) => {
                                 return (
                                     <MenuItem
                                         key={index}
-                                        value={menuItem.value}
-                                        sx={{ minWidth: '406px' }}
+                                        className={classes.menuItem}
+                                        value={menuItem.value || menuItem?.id}
+                                        {...menuItemProps}
                                     >
-                                        {menuItem.text}
+                                        <Typography fontSize={'14px'}>{menuItem.text}</Typography>
+                                        {menuItem.price && <PriceHolder price={menuItem.price} />}
                                     </MenuItem>
                                 )
                             })}
                         </Select>
+                        <FormHelperText error={invalid}>
+                            {invalid ? error?.message : helperText}
+                        </FormHelperText>
                     </FormControl>
                 </GridItem>
             )}
