4e03563b4301a1c6297953a7a83b251ea53df94a
oranjevo <iandrew.jj@gmail.com>
2023-04-26 18:04:26 +0300

    edit

--- src/components/UI/Grid/GridContainer.tsx
+++ src/components/UI/Grid/GridContainer.tsx
@@ -1,5 +1,5 @@
-import { ReactNode } from 'react'
 import Grid2, { Grid2Props } from '@mui/material/Unstable_Grid2'
+import { ReactNode } from 'react'
 
 interface Props extends Grid2Props {
     children: ReactNode
