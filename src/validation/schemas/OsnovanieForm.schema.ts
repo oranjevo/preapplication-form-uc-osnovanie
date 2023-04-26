2435720a0a2707fd9511a2fdfe6b9a9685e7d3d8
oranjevo <iandrew.jj@gmail.com>
2023-04-26 18:06:39 +0300

    add conditional validation

--- src/validation/schemas/OsnovanieForm.schema.ts
+++ src/validation/schemas/OsnovanieForm.schema.ts
@@ -4,11 +4,17 @@ export const osnovanieFormSchema = yup.object({
     partyType: yup.string().required(),
     firstname: yup.string().required().min(2).max(50),
     lastname: yup.string().required().min(2).max(50),
-    inn: yup.string().required().min(12).max(12).isINNIndividual(),
     phone: yup.string().required().isPhoneNumber(),
     email: yup.string().required().email(),
     region: yup.string().required(),
     city: yup.string().required().min(2).max(30),
     product: yup.string().required(),
-    comment: yup.string().max(200)
+    comment: yup.string().max(200),
+    inn: yup
+        .string()
+        .when('partyType', (partyType, schema) =>
+            partyType[0] === '3'
+                ? schema.required().min(10).max(10).isINNCompany()
+                : schema.required().min(12).max(12).isINNIndividual()
+        )
 })
