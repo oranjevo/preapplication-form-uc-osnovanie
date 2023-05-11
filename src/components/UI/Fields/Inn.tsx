31dd3ea94722cd3f4f46d17efe5e2666c79c6241
oranjevo <iandrew.jj@gmail.com>
2023-05-11 13:15:52 +0300

    add mask

--- src/components/UI/Fields/Inn.tsx
+++ src/components/UI/Fields/Inn.tsx
@@ -1,8 +1,37 @@
-import { Field } from 'components/UI/Control/Field'
+import { TextField } from 'components/UI/Control/TextField'
 import { FIELDS } from 'constants/FIELDS'
+import { PartyType } from 'types'
+import { useEffect } from 'react'
+import { useFormContext } from 'react-hook-form'
+import { useFormState } from 'hooks/useFormState'
 
 const Inn = () => {
-    return <Field name={'inn'} type={'text'} fieldParams={FIELDS.INN} />
+    const {
+        setValue,
+        clearErrors,
+        formState: { isDirty }
+    } = useFormContext()
+    const { partyType } = useFormState.useFields()
+    const setFields = useFormState.useSetFields()
+
+    const isCompany = +partyType === PartyType.company
+    const mask = Array(isCompany ? 10 : 12).fill(/\d/)
+
+    useEffect(() => {
+        if (isDirty) {
+            setValue('inn', '')
+            setFields('', 'inn')
+            clearErrors()
+        }
+    }, [partyType, setFields, setValue, clearErrors, isDirty])
+    return (
+        <TextField
+            name={'inn'}
+            type={'text'}
+            mask={mask}
+            fieldParams={isCompany ? FIELDS.INN_COMPANY : FIELDS.INN}
+        />
+    )
 }
 
 export { Inn }
