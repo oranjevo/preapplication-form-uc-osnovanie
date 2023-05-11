72143db06b8b52299ad47972a277e164237e16ae
oranjevo <iandrew.jj@gmail.com>
2023-05-11 13:17:38 +0300

    add event handlers

--- src/components/UI/Fields/Phone.tsx
+++ src/components/UI/Fields/Phone.tsx
@@ -1,5 +1,9 @@
-import { Field } from 'components/UI/Control/Field'
+import { TextField } from 'components/UI/Control/TextField'
 import { FIELDS } from 'constants/FIELDS'
+import { FocusEventHandler } from 'react'
+import { useFormContext } from 'react-hook-form'
+import { IFormFields } from 'types'
+import { useFormState } from 'hooks/useFormState'
 
 const Phone = () => {
     const mask = [
@@ -22,11 +26,25 @@ const Phone = () => {
         /\d/,
         /\d/
     ]
+    const isEmptyField = useFormState.useIsEmptyField()
+    const { setValue } = useFormContext<IFormFields>()
+    const onFocus: FocusEventHandler<HTMLInputElement> = () => {
+        if (isEmptyField('phone')) {
+            setValue('phone', '+7 (')
+        }
+    }
+    const onBlur: FocusEventHandler<HTMLInputElement> = () => {
+        if (isEmptyField('phone')) {
+            setValue('phone', '')
+        }
+    }
     return (
-        <Field
+        <TextField
             name={'phone'}
             type={'tel'}
             mask={mask}
+            onFocus={onFocus}
+            onBlur={onBlur}
             fieldParams={FIELDS.PHONE}
         />
     )
